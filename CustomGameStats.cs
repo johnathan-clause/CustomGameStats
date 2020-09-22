b104d6eedff115f6af72e22b1a094be989d25899
johnathan clause <hashmalum@gmail.com>
2020-09-22 15:31:21 -0500

    updated version

--- CustomGameStats.cs
+++ CustomGameStats.cs
@@ -12,7 +12,7 @@ namespace CustomGameStats
     {
         public const string GUID = "com.theinterstice.customgamestats";
         public const string NAME = "Custom Game Stats";
-        public const string VERSION = "2.1.3";
+        public const string VERSION = "2.1.4";
         public const string DEPENDENT = SharedModConfig.SharedModConfig.GUID;
 
         public static CustomGameStats Instance { get; private set; }
